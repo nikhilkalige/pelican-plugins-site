990a39f3eb613b8b80822bff2a7b4510c19ab558
Lonewolf <nikhilkalige@gmail.com>
2014-05-19 00:15:02 +0530

    Template updated for the latest theme Static files synced

--- theme-sync.sh
+++ theme-sync.sh
@@ -4,3 +4,6 @@
 cp -r theme/stylesheets/ templates/static/css
 # Copy js
 cp -r theme/js/ templates/static/js
+cp theme/bower_components/foundation/js/foundation.min.js templates/static/js/
+cp theme/bower_components/jquery/dist/jquery.min.js templates/static/js/
+cp theme/bower_components/modernizr/modernizr.js templates/static/js/
